ae083ecd64431e449dde4dc4082aed5f0d4fb58e
Nitesh Rajput <47481088+NiteshRajput131@users.noreply.github.com>
2020-08-16 21:56:42 +0530

    Chapter 1 assignment

---
+++ StarPatterns/src/Assignment1.java
@@ -0,0 +1,16 @@
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		
+		Triangle obj1 = new Triangle();
+		pyramid obj2 = new pyramid();
+		
+		System.out.println("Pattern 1 :");
+		obj1.pattern1();
+		System.out.println("Pattern 2 :");
+		obj2.pattern2();
+
+	}
+
+}
---
+++ StarPatterns/src/Triangle.java
@@ -0,0 +1,15 @@
+
+public class Triangle {
+
+	public void pattern1() {
+		
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+				System.out.print("*");
+			System.out.println();
+		}
+		
+	}
+	
+}
